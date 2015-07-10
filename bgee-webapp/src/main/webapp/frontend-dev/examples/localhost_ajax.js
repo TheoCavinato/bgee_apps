ad825d799ab06de62d88a8ded006b3278dcc79ea
fbastian <frederic.bastian@gmail.com>
2015-07-11 00:44:57 +0200

    refactor static example pages

--- bgee-webapp/src/main/webapp/frontend-dev/examples/localhost_ajax.js
+++ bgee-webapp/src/main/webapp/frontend-dev/examples/localhost_ajax.js
@@ -1,3 +1,2 @@
 GLOBAL_PROPS.setBgeeHost('localhost:8080');
 GLOBAL_PROPS.setWebAppPath('/topanat-dev/');
-GLOBAL_PROPS.setImgPath('../../img/');
---
+++ bgee-webapp/src/main/webapp/frontend-dev/examples/static_page_params.js
@@ -0,0 +1,1 @@
+GLOBAL_PROPS.setImgPath('../../img/');
