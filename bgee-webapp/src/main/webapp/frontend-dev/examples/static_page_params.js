fbfd827458327825f8dd16e9a113e8c0e114c1a2
fbastian <frederic.bastian@gmail.com>
2015-07-13 18:01:50 +0200

    add comments

--- bgee-webapp/src/main/webapp/frontend-dev/examples/static_page_params.js
+++ bgee-webapp/src/main/webapp/frontend-dev/examples/static_page_params.js
@@ -2,4 +2,6 @@
 //relative to the examples directory.
 GLOBAL_PROPS.setImgPath('../../img/');
 //the topAnat test webapp is deployed to the path 'topanat-dev'.
+//This setting would not be needed if you were accessing this page from a server, 
+//but it is needed to perform AJAX queries from pages loaded from file system.
 GLOBAL_PROPS.setWebAppPath('/topanat-dev/');
