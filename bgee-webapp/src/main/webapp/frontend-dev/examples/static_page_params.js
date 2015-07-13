945d7268a88a7b57a7dfd32ebab4a83b617ab76b
fbastian <frederic.bastian@gmail.com>
2015-07-13 14:33:29 +0200

    add comment in js example file

--- bgee-webapp/src/main/webapp/frontend-dev/examples/static_page_params.js
+++ bgee-webapp/src/main/webapp/frontend-dev/examples/static_page_params.js
@@ -1,2 +1,5 @@
+//points to the img directory of the Bgee webapp, 
+//relative to the examples directory.
 GLOBAL_PROPS.setImgPath('../../img/');
+//the topAnat test webapp is deployed to the path 'topanat-dev'.
 GLOBAL_PROPS.setWebAppPath('/topanat-dev/');
